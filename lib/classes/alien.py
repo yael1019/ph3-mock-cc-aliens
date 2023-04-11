18782462142a79c94608b3bfcc851bc691502776
Yael Harosh <yaelharosh01@gmail.com>
2023-04-11 14:14:44 -0400

    plants

--- lib/classes/alien.py
+++ lib/classes/alien.py
@@ -2,34 +2,107 @@ from lib import CONN, CURSOR
 
 class Alien:
 
-    def __init__(self, first_name, last_name, age):
-        pass
+    def __init__(self, first_name, last_name, age, id = None):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self._age = age
 
+    def __repr__(self) -> str:
+        return f'< Alien id = {self.id} first_name = {self.first_name} last_name = {self.last_name} age = {self.age} >'
+    
     # age property goes here
+    def get_age(self):
+        return self._age
+    
+    def set_age(self, age):
+        if isinstance(age, int) and age >= 0:
+            self._age = age
+        else:
+            print('Age must be a number greater or equal to zero')
+
+    age = property(get_age, set_age)
 
     def full_name(self):
-        pass
+        return f'{self.first_name} {self.last_name}'
 
     def save(self):
-        # updates the alien in the database
-        pass
+        if self.id:
+            self.update()
+        else:
+            self.create()
+    
+    def create(self):
+        sql = """
+            INSERT INTO aliens (first_name, last_name, age)
+            VALUES (?, ?, ?)
+        """
+        CURSOR.execute(sql, [self.first_name, self.last_name, self.age])
+        CONN.commit()
+        self.id = CURSOR.execute('SELECT * FROM aliens ORDER BY id DESC LIMIT 1').fetchone()[0]
+
+    def update(self):
+        sql = """
+            UPDATE aliens 
+            SET first_name = ?, last_name = ?, age =?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, [self.first_name, self.last_name, self.age, self.id])
+        CONN.commit()
+
 
     @classmethod
     def query_all(cls):
-        # gets all aliens in the database and returns a list of aliens as instances
-        pass
+        sql = """
+            SELECT * FROM aliens
+        """
+        all = CURSOR.execute(sql).fetchall()
+        result = [Alien(data[1], data[2], data[3], data[0]) for data in all]
+        return result
 
     @classmethod
     def query_one(cls, id):
-        # gets an alien in the database by its id and returns that alien as an instance
-        pass
+        sql = """
+            SELECT * FROM aliens 
+            WHERE id = ?
+        """
+        one = CURSOR.execute(sql, [id]).fetchone()
+        if one:
+            alien = Alien(one[1], one[2], one[3])
+            alien.id = one[0]
+            return alien
+        else:
+            print('This alien does not exist')
+
+    def destroy(self):
+        sql = """
+            DELETE FROM aliens
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, [self.id])
+        CONN.commit()
+
 
     @classmethod
     def average_age(cls):
-        # creates and returns an average age of all aliens
-        pass
+        sql = """
+            SELECT age 
+            FROM aliens
+        """
+        age_list = CURSOR.execute(sql).fetchall()
+        ages = [age[0] for age in age_list]
+        avg = sum(ages) / len(ages)
+        return avg
 
     @classmethod
     def query_oldest(cls):
-        # returns the oldest alien by age as an instance
-        pass
+        sql = """
+            SELECT * FROM aliens 
+            WHERE age = (SELECT MAX(age) FROM aliens)
+        """
+        oldest = CURSOR.execute(sql).fetchone()
+        oldest_repr = Alien(oldest[1], oldest[2], oldest[3], oldest[0])
+        return oldest_repr
+
+
+jane = Alien('Jane', 'Doe', 40)
--- lib/classes/planet.py
+++ lib/classes/planet.py
@@ -2,14 +2,49 @@ from lib import CONN, CURSOR
 
 class Planet:
 
-    def __init__(self, name):
-        pass
+    def __init__(self, name, population, id = None):
+        self.id = id
+        self._name = name 
+        self.population = population
 
     # name property goes here
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 3 <= len(name) <= 15:
+            self._name = name 
+        else:
+            print('Name must be a string between 3 and 15 characters')
+
+    name = property(get_name, set_name)
 
     def save(self):
-        # updates the planet in the database
-        pass
+        if self.id:
+            self.update()
+        else:
+            self.create()
+
+
+
+    def create(self):
+        sql = """
+            INSERT INTO planets (name)
+            VALUES (?)
+        """
+        CURSOR.execute(sql, [self.name])
+        CONN.commit()
+        self.id = CURSOR.execute('SELECT * FROM planets ORDER BY id DESC LIMIT 1').fetchone()[0]
+
+    def update(self):
+        sql = """
+            UPDATE planets
+            SET name = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, [self.name, self.id])
+        CONN.commit()
+
 
     @classmethod
     def query_one(cls, id):
